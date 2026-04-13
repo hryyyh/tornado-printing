440f9b0a3998f6a149bc8edbb338c98cf1a95ce9
marsya <160198348+hryyyh@users.noreply.github.com>
2026-04-13 12:59:00 +0700

    backup sebelum ubah ke database CMS

---
+++ app/Filament/Resources/Products/Tables/ProductsTable.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Filament\Resources\Products\Tables;
+
+use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Actions\EditAction;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+
+class ProductsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('Name')
+                    ->searchable(),
+
+                TextColumn::make('description')
+                    ->label('Description')
+                    ->limit(50),
+
+                TextColumn::make('created_at')
+                    ->label('Created')
+                    ->dateTime(),
+            ])
+            ->actions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->bulkActions([
+                DeleteBulkAction::make(),
+            ]);
+    }
+}
